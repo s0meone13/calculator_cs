6cd54b9abbbf670767793dfad8df86e080548227
fffff1 <ildarsafianov@gmail.com>
2024-03-30 11:54:48 +0400

    add division

---
+++ Division.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    public static class Division
+    {
+        public static void PerformOperation(int a, int b)
+        {
+            if (b != 0)
+            {
+                Console.WriteLine($"Результат деления A / B: {(double)a / b}");
+            }
+            else
+            {
+                Console.WriteLine("Деление на ноль невозможно.");
+            }
+        }
+    }
+}
