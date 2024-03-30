89cfd4538f7986307a5ff236df53213d41ba36ee
fffff1 <ildarsafianov@gmail.com>
2024-03-30 12:00:48 +0400

    add InputB

--- InputB.cs
+++ InputB.cs
@@ -6,13 +6,13 @@ using System.Threading.Tasks;
 
 namespace calculator
 {
-    public static class InputA
+    public static class InputB
     {
         public static void InputValue(ref int value, string message)
         {
             Console.WriteLine(message);
             value = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Вы ввели A = " + value);
+            Console.WriteLine("Вы ввели B = " + value);
         }
     }
 }
