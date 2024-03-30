90bcc323524504e3e12f6174c2511f854e748422
fffff1 <ildarsafianov@gmail.com>
2024-03-30 11:59:33 +0400

    add InputA

---
+++ InputA.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    public static class InputA
+    {
+        public static void InputValue(ref int value, string message)
+        {
+            Console.WriteLine(message);
+            value = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Вы ввели A = " + value);
+        }
+    }
+}
